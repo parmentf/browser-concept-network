8f9abe43d8f9ca9023edeae2176207b044c72f8a
François Parmentier <francois.parmentier@gmail.com>
2013-01-13 23:35:01 +0100

    Display loaded Concept Network.

--- js/concept-network.js
+++ js/concept-network.js
@@ -3,11 +3,52 @@ $(document).ready(function() {
   var save = $('#save');
   var name = $('#name');
 
-  // var ConceptNetwork = require('concept-network');
+  // var ConceptNetwork = require('concept-network').ConceptNetwork; // FIXME
   var ConceptNetwork = require('/home/francois/dev/browser-concept-network/lib/concept-network').ConceptNetwork;
   cn = new ConceptNetwork();
-  cn.addNode('Test');
-  console.log(cn);
+
+  if (!localStorage) {
+    load.remove();
+    save.remove();
+  }
+  else {
+    // save.hide();
+    if (typeof localStorage.ector === 'undefined') {
+      load.hide();
+    }
+  }
+
+  var alertTpl = $('#alerts').html();
+  $('#alerts div').remove();
+  /**
+   *
+   * @param {string} type 'success', 'warning', 'error', or 'info'
+   * @param {string} message
+   **/
+  var showAlert = function(type, message) {
+    var inform = {
+      type: type,
+      message: message
+    };
+    $('#alerts').append(Mustache.render(alertTpl, inform));
+  };
+
+  var infoTpl = $('#infos').html();
+  $('#infos').html('');
+  var showInfos = function() {
+    var status = {
+      nodes_number: cn.nodeLastId,
+      links_number: Object.keys(cn.link).length
+    };
+    // status.nodes = [
+    //   {id: 1, label: "sSalut, comment ça va?", occ: 1}
+    // ];
+    status.nodes = [];
+    for(var i in cn.node) {
+      status.nodes.push(cn.node[i]);
+    }
+    $('#infos').append(Mustache.render(infoTpl, status));
+  };
 
   load.on('click', function () {
     var cnSave = localStorage.ector;
@@ -15,7 +56,18 @@ $(document).ready(function() {
     Object.merge(newCN, JSON.parse(cnSave));
     cn = newCN;
     load.hide();
-    alert('Concept Network loaded.');
+    showAlert('success', 'Concept Network loaded.');
+    showInfos();
     return false;
   });
+
+  save.on('click', function () {
+    localStorage.setItem('ector', JSON.stringify(cn));
+    load.hide();
+    save.hide();
+
+    showAlert('success', 'Concept Network saved.');
+    return false;
+  });
+
 });
